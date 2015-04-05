7012cbbc31a37272c31a350177e1ab1234d434ea
Victor <victorhugo.vhrn@gmail.com>
2015-04-05 15:07:13 -0300

    Protótipo do Jogo da Velha. Contém:
    - Arquitetura menos acoplada primitiva
    - Loop principal do jogo implementado
    - Eventos principais implementados
    
    Adicionados:
    
    - Arquivo com exemplos do Pygame
    - Arquivos de recursos de som e imagens
    - Módulo com constantes

---
+++ constantes.py
@@ -0,0 +1,8 @@
+import pygame
+WHITE       = pygame.Color(255,255,255)
+RED         = pygame.Color(255,0,0)
+GREEN       = pygame.Color(0,255,0)
+BLUE        = pygame.Color(0,0,255)
+retangulo   = pygame.Rect((180,100,40,40))
+JOGADOR1    = 0
+JOGADOR2    = 1
