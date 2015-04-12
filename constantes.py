779daa9c62e4ed558e3edca5504991d04949c0d6
Victor <victorhugo.vhrn@gmail.com>
2015-04-12 14:06:33 -0300

    Adicionado arquivo de configuração do Jogo.

--- constantes.py
+++ constantes.py
@@ -1,5 +1,6 @@
 import pygame
 BLACK       = pygame.Color(0,0,0)
+CONF_PATH   = "conf/elder.conf"
 WHITE       = pygame.Color(255,255,255)
 RED         = pygame.Color(255,0,0)
 GREEN       = pygame.Color(0,255,0)
